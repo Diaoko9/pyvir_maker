bb355e564eac54722a3364d6498d5ca73bc3291c
Diaoko <ahmad.p.92@gmail.com>
2023-07-03 13:48:38 +0330

    merge some code add ui

--- dir_display.py
+++ dir_display.py
@@ -1,7 +1,14 @@
 import os
-import dir_changer
 
-dir_changer
+venv_dir = "/home/diaoko/venv"
+cur_dir = os.getcwd()
+
+
+if (cur_dir != venv_dir) :
+    os.chdir(venv_dir)
+
+
 q1 = os.listdir()
 
-print(q1)
+for i in q1 :
+    print(i)
